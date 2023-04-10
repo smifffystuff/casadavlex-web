b837432a8ce0d095a67cdf3db1b4746ea4317826
Martin <martin.smifff@gmail.com>
2023-04-10 10:53:42 +0100

    Pass in a variable via environment

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -18,9 +18,21 @@ provider "aws" {
     region = "eu-west-2"
 }
 
+variable "host_name" {
+  default = "temp"
+}
+
 resource "aws_route53_record" "www" {
   zone_id = data.aws_route53_zone.davlex.zone_id
   name = "www.${data.aws_route53_zone.davlex.name}"
+  type = "A"
+  ttl = "600"
+  records = ["10.0.0.2"]
+}
+
+resource "aws_route53_record" "test_host" {
+  zone_id = data.aws_route53_zone.davlex.zone_id
+  name = "${var.host_name}.${data.aws_route53_zone.davlex.name}"
   type = "A"
   ttl = "600"
   records = ["10.0.0.2"]
